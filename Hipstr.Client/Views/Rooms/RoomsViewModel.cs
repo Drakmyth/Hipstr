f3144019c49b21f23febac532b0169276fe76850
Drakmyth <shaun.hamman@gmail.com>
2016-12-15 15:02:54 -0800

    Add letter group headers to room list

--- Hipstr.Client/Views/Rooms/RoomsViewModel.cs
+++ Hipstr.Client/Views/Rooms/RoomsViewModel.cs
@@ -1,11 +1,13 @@
 ﻿using Hipstr.Client.Commands;
 using Hipstr.Client.Views.Messages;
+using Hipstr.Core.Comparers;
 using Hipstr.Core.Models;
 using Hipstr.Core.Services;
 using Hipstr.Core.Utility.Extensions;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
@@ -13,10 +15,10 @@ namespace Hipstr.Client.Views.Rooms
 {
 	public class RoomsViewModel : ViewModelBase
 	{
-		private readonly IHipChatService _hipChatService;
-
 		public ObservableCollection<RoomGroup> RoomGroups { get; }
 		public ICommand NavigateToMessagesViewCommand { get; }
+
+		private readonly IHipChatService _hipChatService;
 
 		public RoomsViewModel() : this(IoCContainer.Resolve<IHipChatService>())
 		{
@@ -34,12 +36,26 @@ namespace Hipstr.Client.Views.Rooms
 		{
 			IEnumerable<Room> rooms = await _hipChatService.GetRoomsAsync();
 
-			IEnumerable<RoomGroup> roomGroups = rooms.OrderBy(room => room.Name)
-				.GroupBy(room => room.Name[0].ToString(), room => room,
+			IEnumerable<RoomGroup> roomGroups = rooms.OrderBy(room => room.Name, RoomNameComparer.Instance)
+				.GroupBy(room => DetermineGroupHeader(room.Name), room => room,
 					(group, groupedRooms) => new RoomGroup(group, groupedRooms));
 
 			RoomGroups.Clear();
 			RoomGroups.AddRange(roomGroups);
+		}
+
+		private string DetermineGroupHeader(string name)
+		{
+			string nameFirstChar = name[0].ToString();
+
+			var alphaRegex = new Regex("^[A-Za-z]$");
+			if (alphaRegex.IsMatch(nameFirstChar))
+			{
+				return nameFirstChar.ToUpper();
+			}
+
+			var numRegex = new Regex("^[0-9]$");
+			return numRegex.IsMatch(nameFirstChar) ? "#" : "?";
 		}
 	}
 }
