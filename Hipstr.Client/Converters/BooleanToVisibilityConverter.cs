af88390b7b25194ba58bbe952a9c356d72247485
Drakmyth <shaun.hamman@gmail.com>
2017-01-18 08:08:32 -0800

    Show/Hide Emoticon and Refresh Messages buttons based on keyboard visibility

---
+++ Hipstr.Client/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Hipstr.Client.Converters
+{
+	public class BooleanToVisibilityConverter : IValueConverter
+	{
+		public bool InvertResult { get; set; } = false;
+
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			var val = (bool)value;
+			if (InvertResult)
+			{
+				val = !val;
+			}
+			return val ? Visibility.Visible : Visibility.Collapsed;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			bool val = (Visibility)value == Visibility.Visible;
+			if (InvertResult)
+			{
+				val = !val;
+			}
+			return val;
+		}
+	}
+}
--- Hipstr.Client/Views/Messages/MessagesView.xaml.cs
+++ Hipstr.Client/Views/Messages/MessagesView.xaml.cs
@@ -1,26 +1,48 @@
 ﻿using Hipstr.Core.Messaging;
+using JetBrains.Annotations;
 using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Windows.UI.Core;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
 namespace Hipstr.Client.Views.Messages
 {
-	public sealed partial class MessagesView : Page
+	public sealed partial class MessagesView : Page, INotifyPropertyChanged
 	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
 		public MessagesViewModel ViewModel => (MessagesViewModel)DataContext;
 		private bool _pollForMessages;
+
+		private bool _inputPaneVisible;
+
+		public bool IsInputPaneVisible
+		{
+			get { return _inputPaneVisible; }
+			private set
+			{
+				_inputPaneVisible = value;
+				OnPropertyChanged();
+			}
+		}
 
 		public MessagesView()
 		{
 			InitializeComponent();
 			DataContext = IoCContainer.Resolve<MessagesViewModel>();
+			_inputPaneVisible = false;
 			_pollForMessages = true;
 		}
 
 		protected override async void OnNavigatedTo(NavigationEventArgs e)
 		{
+			InputPane.GetForCurrentView().Showing += InputPane_OnShowing;
+			InputPane.GetForCurrentView().Hiding += InputPane_OnHiding;
+
 			ViewModel.MessageSource = (IMessageSource)e.Parameter;
 			await ViewModel.ReloadMessagesAsync();
 
@@ -33,7 +55,26 @@ namespace Hipstr.Client.Views.Messages
 
 		protected override void OnNavigatedFrom(NavigationEventArgs e)
 		{
+			InputPane.GetForCurrentView().Showing -= InputPane_OnShowing;
+			InputPane.GetForCurrentView().Hiding -= InputPane_OnHiding;
+
 			_pollForMessages = false;
+		}
+
+		private void InputPane_OnShowing(InputPane sender, InputPaneVisibilityEventArgs args)
+		{
+			IsInputPaneVisible = true;
+		}
+
+		private void InputPane_OnHiding(InputPane sender, InputPaneVisibilityEventArgs args)
+		{
+			IsInputPaneVisible = false;
+		}
+
+		[NotifyPropertyChangedInvocator]
+		private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 		}
 	}
 }
