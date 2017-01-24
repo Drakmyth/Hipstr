3dfbd1496894b7d77460a01e72d54619e148e7f6
Drakmyth <shaun.hamman@gmail.com>
2017-01-24 08:33:03 -0800

    Removed duplicated error handling in HttpClient. Added rate limit check.

--- Hipstr.Core/Services/HipstrHttpClient.cs
+++ Hipstr.Core/Services/HipstrHttpClient.cs
@@ -1,6 +1,8 @@
 ﻿using JetBrains.Annotations;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.ServiceModel;
@@ -22,51 +24,21 @@ namespace Hipstr.Core.Services
 
 		public async Task<HttpResponseMessage> GetAsync(Uri requestUri)
 		{
-			try
-			{
-				HttpResponseMessage response = await _httpClient.GetAsync(requestUri);
-				if (!response.IsSuccessStatusCode)
-				{
-					// HipChat returned a non-200 range status code. We shouldn't have given the user an action that
-					// wasn't valid, so we report the error.
-
-					// TODO: Add automatic error reporting
-					/*
-					 * string errorMessage = "Received an error response from the HipChat server.";
-					 * 
-					 * if (telemetryEnabled) {
-					 *	   sendErrorReport(response);
-					 *     errorMessage += " An error report has been sent to the developer.";
-					 * }
-					 * 
-					 * errorMessage = " Please go back, refresh the page, and try again.";
-					 */
-
-					throw new CommunicationException($"Received an error response from the HipChat server. Code: {response.StatusCode} - Reason: {response.Content}");
-				}
-
-				if (!response.RequestMessage.RequestUri.Equals(requestUri))
-				{
-					// Our request got redirected. This means we probably hit WebSense or something similar, so let's fail gracefully.
-					throw new CommunicationException($"The request to the HipChat server was intercepted, possibly by a Web Filter. Please allow {requestUri.DnsSafeHost} as an exception in the filter's whitelist.");
-				}
-
-				return response;
-			}
-			catch (HttpRequestException e)
-			{
-				// We failed to connect to the HipChat server. This most likely means the user has no internet connection.
-				throw new CommunicationException("Communication with the HipChat server has failed. Please check your internet connection and try again.", e);
-			}
+			return await SendRequestAsync(requestUri, async () => await _httpClient.GetAsync(requestUri))
 		}
 
 		public async Task<HttpResponseMessage> PostAsync<T>(Uri requestUri, T payload)
 		{
+			HttpContent content = new StringContent(JsonConvert.SerializeObject(payload));
+			content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+			return await SendRequestAsync(requestUri, async () => await _httpClient.PostAsync(requestUri, content));
+		}
+
+		private static async Task<HttpResponseMessage> SendRequestAsync(Uri requestUri, Func<Task<HttpResponseMessage>> clientRequestMethod)
+		{
 			try
 			{
-				HttpContent content = new StringContent(JsonConvert.SerializeObject(payload));
-				content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-				HttpResponseMessage response = await _httpClient.PostAsync(requestUri, content);
+				HttpResponseMessage response = await clientRequestMethod.Invoke();
 
 				if (!response.RequestMessage.RequestUri.Equals(requestUri))
 				{
@@ -90,6 +62,11 @@ namespace Hipstr.Core.Services
 					 * 
 					 * errorMessage = " Please go back, refresh the page, and try again.";
 					 */
+					IEnumerable<string> rateLimitHeader;
+					if (response.Headers.TryGetValues("X - Ratelimit - Remaining", out rateLimitHeader) && int.Parse(rateLimitHeader.First()) <= 0)
+					{
+						throw new CommunicationException($"Reached HipChat rate limit. Please wait 5 minutes then try again.");
+					}
 
 					throw new CommunicationException($"Received an error response from the HipChat server. Code: {response.StatusCode} - Reason: {response.Content}");
 				}
