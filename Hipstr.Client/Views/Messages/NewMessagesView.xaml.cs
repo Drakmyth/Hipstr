9fe604e38df7b27183b45d75059dae33ecae7d7b
Drakmyth <shaun.hamman@gmail.com>
2017-10-09 08:51:09 -0700

    Started NewMessagesView

---
+++ Hipstr.Client/Views/Messages/NewMessagesView.xaml.cs
@@ -0,0 +1,84 @@
+﻿using Hipstr.Client.Commands;
+using Hipstr.Core.Messaging;
+using JetBrains.Annotations;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace Hipstr.Client.Views.Messages
+{
+	public sealed partial class NewMessagesView : Page//, INotifyPropertyChanged
+	{
+//		public event PropertyChangedEventHandler PropertyChanged;
+
+		public NewMessagesViewModel ViewModel => (NewMessagesViewModel)DataContext;
+//		public ICommand ToggleEmoticonPaneCommand { get; }
+//
+//		private bool _inputPaneVisible;
+//
+//		public bool IsInputPaneVisible
+//		{
+//			get => _inputPaneVisible;
+//			private set
+//			{
+//				_inputPaneVisible = value;
+//				ShowEmoticonPane = ShowEmoticonPane; // Refresh ShowEmoticonPane in case IsInputPanelVisible changed
+//				OnPropertyChanged();
+//			}
+//		}
+//
+//		private bool _showEmoticonPane;
+//
+//		public bool ShowEmoticonPane
+//		{
+//			get => _showEmoticonPane;
+//			set
+//			{
+//				_showEmoticonPane = value && IsInputPaneVisible;
+//				OnPropertyChanged();
+//			}
+//		}
+
+		public NewMessagesView()
+		{
+			InitializeComponent();
+			DataContext = IoCContainer.Resolve<MessagesViewModel>();
+//			_inputPaneVisible = false;
+//			_showEmoticonPane = false;
+//			ToggleEmoticonPaneCommand = new RelayCommand(() => ShowEmoticonPane = !ShowEmoticonPane);
+		}
+
+		protected override void OnNavigatedTo(NavigationEventArgs e)
+		{
+//			InputPane.GetForCurrentView().Showing += InputPane_OnShowing;
+//			InputPane.GetForCurrentView().Hiding += InputPane_OnHiding;
+
+			ViewModel.MessageSource = (IMessageSource)e.Parameter;
+		}
+
+		protected override void OnNavigatedFrom(NavigationEventArgs e)
+		{
+//			InputPane.GetForCurrentView().Showing -= InputPane_OnShowing;
+//			InputPane.GetForCurrentView().Hiding -= InputPane_OnHiding;
+		}
+//
+//		private void InputPane_OnShowing(InputPane sender, InputPaneVisibilityEventArgs args)
+//		{
+//			IsInputPaneVisible = true;
+//		}
+//
+//		private void InputPane_OnHiding(InputPane sender, InputPaneVisibilityEventArgs args)
+//		{
+//			IsInputPaneVisible = false;
+//		}
+//
+//		[NotifyPropertyChangedInvocator]
+//		private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+//		{
+//			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+//		}
+	}
+}
